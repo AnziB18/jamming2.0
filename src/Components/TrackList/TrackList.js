71cd35edb8b82c65d203c8d138ee5c046c03d7da
Bernd Anzenberger <anzenbernd@gmail.com>
2022-05-31 19:24:00 +0200

    add map func for tracks

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,6 +5,9 @@ class TrackList extends React.Component {
     render(){
         return (
             <div className="TrackList">
+                {this.props.tracks.map((track) =>
+                <Track  key={track.id} name={track.name} album={track.album} artist = {track.artist}/>
+                )}
                 {/* <!-- You will add a map method that renders a set of Track components  --> */}
             </div>
         )
