f71c420c147dc08be8d83b8feebfd88350bfd419
Bernd Anzenberger <anzenbernd@gmail.com>
2022-05-31 18:50:35 +0200

    add renderAction func, corr class to className

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,14 +2,18 @@ import React from "react";
 import './Track.css';
 
 class Track extends React.Component {
+    renderAction(){
+        let isRemoval;
+        isRemoval ? <button className="Track-action">-</button> : <button className="Track-action">+</button>;
+    }
     render(){
         return (
-            <div class="Track">
-                <div class="Track-information">
+            <div className="Track">
+                <div className="Track-information">
                     {/* <h3><!-- track name will go here --></h3> */}
                     {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
                 </div>
-                {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+                {/* <button className="Track-action"><!-- + or - will go here --></button> */}
             </div>
         )
     }
