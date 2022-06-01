94de7b4963ed66f7779a51762ba6d81fe260f1bd
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-01 17:35:57 +0200

    added constructor, search func, event handler

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Track from "../Track/Track";
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {term: []}
+        this.search = this.search.bind(this);
+    }
+    search(term){
+        this.props.onSearch(term);
+    }
+    handleTermChange(e){
+        const changeTerm = e.target.value;
+        this.setState({term: changeTerm})
+    }
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" onChange = {this.handleTermChange}/>
+                <button className="SearchButton" onClick = {this.search}>SEARCH</button>
             </div> 
         )
     }
