8f124bca92bf4462ee0c5730be397de4c11a2ef4
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-02 10:02:23 +0200

    added search func to get results from spotify API

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,7 @@
 let userAccessToken;
+const client_id = 'fa6047bbfec8400b9997ea510df9cc9c';
+const redirect_uri = 'http://localhost:3000/';
+let searchUrl = 'https://api.spotify.com/v1/search?';
 const generateRandomString= (length) => {
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let text = '';
@@ -22,20 +25,42 @@ const Spotify ={
             window.history.pushState('Access Token', null, '/')
             return userAccessToken
         } else {
-            const client_id = 'fa6047bbfec8400b9997ea510df9cc9c';
-            const redirect_uri = 'http://localhost:3000/';
             const stateKey = 'spotify_auth_state';
             const state = generateRandomString(16);
             
             localStorage.setItem(stateKey, state);
             const scope = 'user-read-private user-read-email';
-            let url = 'https://accounts.spotify.com/authorize';
-            url += '?response_type=token';
-            url += '&client_id=' + encodeURIComponent(client_id);
-            url += '&scope=' + encodeURIComponent(scope);
-            url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-            url += '&state=' + encodeURIComponent(state);
-            window.location = url;
+            let accesUrl = 'https://accounts.spotify.com/authorize';
+            accesUrl += '?response_type=token';
+            accesUrl += '&client_id=' + encodeURIComponent(client_id);
+            accesUrl += '&scope=' + encodeURIComponent(scope);
+            accesUrl += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+            accesUrl += '&state=' + encodeURIComponent(state);
+            window.location = accesUrl;
+        }
+    },
+    async search(term){
+        const type = 'type=track';
+        const externAudio = '&include_external=audio';
+        const q = '&q=TERM';
+        const limit ='&10';
+        searchUrl += `${type}${externAudio}${limit}${q}`;
+        
+        try{
+            const accessToken = Spotify.getAccessToken();
+            const headers = {Authorization: `Bearer ${accessToken}`};
+            const response = await fetch(searchUrl,headers);
+            if(response.ok){
+                const jsonResponse = await response.json()
+                if(!jsonResponse.tracks){
+                    return [];
+                } else {
+                return jsonResponse.tracks.item.map(track => ({name: track.name, uri: track.uri, id: track.id, artist: track.artists[0].name, album: track.ablum.name} ) ) // map through an array, get a new array, but inside this new array, we need a object for app.js to handle searchResults
+                }
+            }
+            throw new Error('Request failed');
+        } catch(error){
+            console.log(error);
         }
     }
 };
