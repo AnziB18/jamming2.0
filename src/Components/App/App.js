17c272e41858dc1ce614524f2f17dfb872f3c5cc
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-03 15:50:23 +0200

    call Spotify.getAccesToken to fix a render bug

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {searchResults: [
-                          {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
-                          {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-                          {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+                            {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
+                            {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+                            {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
                           ],
                   playlistName: 'New Playlist',
                   playlistTracks:
@@ -27,6 +27,7 @@ class App extends React.Component {
     this.savePlaylist = this.savePlaylist.bind(this);
     this.search = this.search.bind(this);
   }
+
   addTrack(track){
     const checkID = this.state.playlistTracks;
     if(this.state.playlistTracks.find(checkedID => checkedID.id === track.id)){
@@ -48,18 +49,19 @@ class App extends React.Component {
   }
   search(term){
     Spotify.search(term).then(searchResults => { // have to chain the promise from Spotify module
-      this.setState({searchResults: searchResults})
+      return this.setState({searchResults: searchResults})
       })
   }
   render(){
+    Spotify.getAccessToken(); // this pretends the app to re-render, because it initialize the accesToken for first render too
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar onSearch = {this.search}/>
           <div className="App-playlist">
-            <SearchResults  searchResults={this.state.searchResults} 
-                            onAdd={this.addTrack}
+          <SearchResults  searchResults={this.state.searchResults} 
+                          onAdd={this.addTrack}
                             />
             <Playlist playlistName = {this.state.playlistName} 
                       playlistTracks = {this.state.playlistTracks}
