355405d071127135019dc06da00f8919df6a342a
Bernd Anzenberger <anzenbernd@gmail.com>
2022-06-04 08:51:25 +0200

    comm out hardcoded value, fixed savePlaylist func

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,15 +10,15 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {searchResults: [
-                            {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
-                            {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-                            {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+                            // {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
+                            // {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+                            // {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
                           ],
                   playlistName: 'New Playlist',
-                  playlistTracks:
-                          [{name: 'playlistname4', artist: 'playlistartist4', album: 'playlistalbum4', id: 4},
-                          {name: 'playlistname5', artist: 'playlistartist5', album: 'playlistalbum5', id: 5},
-                          {name: 'playlistname6', artist: 'playlistartist6', album: 'playlistalbum6', id: 6}
+                  playlistTracks:[
+                          // [{name: 'playlistname4', artist: 'playlistartist4', album: 'playlistalbum4', id: 4},
+                          // {name: 'playlistname5', artist: 'playlistartist5', album: 'playlistalbum5', id: 5},
+                          // {name: 'playlistname6', artist: 'playlistartist6', album: 'playlistalbum6', id: 6}
                           ]
                   }
     this.addTrack = this.addTrack.bind(this);
@@ -45,8 +45,13 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
   savePlaylist(){
-    const trackURIs = this.state.playlistTracks.id
-  }
+    const trackUris= this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []})
+    })
+    }
   search(term){
     Spotify.search(term).then(searchResults => { // have to chain the promise from Spotify module
       return this.setState({searchResults: searchResults})
